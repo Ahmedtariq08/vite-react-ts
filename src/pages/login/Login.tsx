fa7a210d9aaafd348172f63627365c22ad8a96ae
ahmed.tariq <ahmed.tariq@gosaas.io>
2023-06-19 21:54:59 +0500

    styling complete for login

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,5 +1,23 @@
 import { Button } from '@mui/material';
+import TextField from '@mui/material/TextField';
 import './login.css';
+
+function LoginFooter() {
+  return (
+    <footer className="login-footer">
+      <div className="footer-logo">
+        <img
+          className="logo"
+          src="src/assets/images/gosaas-logo.png"
+          alt="src/assets/images/gosaas-logo.png"
+        />
+      </div>
+      <div className="copyright" id="copyright">
+        Copyright(C) 2017, 2022, GoSaaS and/or its affiliates.
+      </div>
+    </footer>
+  );
+}
 
 function Login() {
   return (
@@ -8,20 +26,57 @@ function Login() {
         <div className="head1">Sign In</div>
         <div className="head2">Environment Government & Compliance</div>
       </div>
-      <div className="input-fields-container" />
-      <div />
-      <footer className="login-footer">
-        <div className="footer-logo">
-          <img
-            className="logo"
-            src="src/assets/images/gosaas-logo.png"
-            alt="src/assets/images/gosaas-logo.png"
+      <div className="input-fields-container">
+        <div className="buttons-container">
+          <Button
+            color="primary"
+            variant="contained"
+            size="large"
+            sx={{
+              textTransform: 'none',
+              fontWeight: 600,
+              fontSize: 'medium',
+            }}
+          >
+            Company Single Sign On
+          </Button>
+          <div className="or-line-container">
+            <hr className="hr1" />
+            <span>or</span>
+            <hr className="hr2" />
+          </div>
+          <TextField
+            color="primary"
+            variant="outlined"
+            label="Username"
+            type="text"
+            spellCheck={false}
+            required
           />
+          <TextField
+            color="primary"
+            variant="outlined"
+            label="Password"
+            type="password"
+            spellCheck={false}
+            required
+          />
+          <Button
+            color="secondary"
+            variant="contained"
+            className="sign-in-button"
+            size="large"
+            sx={{
+              textTransform: 'none',
+              fontWeight: 600,
+              fontSize: 'medium',
+            }}
+          >
+            Sign In
+          </Button>
         </div>
-        <div className="copyright" id="copyright">
-          Copyright(C) 2017, 2022, GoSaaS and/or its affiliates.
-        </div>
-      </footer>
+      </div>
+      <LoginFooter />
     </div>
   );
 }
--- src/styles/mui/theme.ts
+++ src/styles/mui/theme.ts
@@ -3,17 +3,21 @@ import { ThemeOptions, createTheme } from '@mui/material/styles';
 // background black: rgb(49,45,42)
 // grey-white-bg: rgb(251, 249, 248)
 // card-grey: rgb(92,86,81)
+// test grey color #e0e0e0
 const theme: ThemeOptions = createTheme({
   palette: {
     mode: 'light',
     primary: {
-      main: '#312d2a',
+      main: '#312d2a', // calltoActionBlack
     },
     secondary: {
+      main: '#e0e0e0',
+    },
+    info: {
       main: '#5c5651',
     },
     background: {
-      default: '#fbf9f8',
+      default: '#fbf9f8', // normal grey white
     },
   },
 });
